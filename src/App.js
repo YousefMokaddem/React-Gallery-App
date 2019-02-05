d04c2e10568edd0b6c803aa1846ab9d639c50ad8
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-02-04 23:31:51 -0500

    Search functionality
    search listener and api request

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 } from 'react-router-dom';
 class App extends Component {
   state = {
-    searched:{
+    search:{
       urls: [],
       loading: true
     },
@@ -25,6 +25,19 @@ class App extends Component {
       urls: [],
       loading: true
     }
+  }
+  handleSearch(q){
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${q}&format=json&nojsoncallback=1`)
+      .then(res=>res.json())
+      .then(res=>res.photos.photo.map(photo => `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`))
+      .then(res=>this.setState({
+        search:{
+          urls: res,
+          loading: false
+        }
+      }))
+      .catch(err => console.log('error fetching and parsing data', err));
+      
   }
   componentDidMount(){
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&format=json&nojsoncallback=1`)
@@ -59,11 +72,12 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <SearchForm />
+          <Route render={() => <SearchForm search={this.handleSearch.bind(this) }/>} />
           <MainNav />
           <Route path="/cats" render={() => <PhotoContainer urls={this.state.cats.urls} loading = {this.state.cats.loading}/>} />
           <Route path="/dogs" render={() => <PhotoContainer urls={this.state.dogs.urls} loading = {this.state.dogs.loading}/>} />
           <Route path="/computers" render={() => <PhotoContainer urls={this.state.computers.urls} loading = {this.state.computers.loading}/>} />
+          <Route path="/search" render={() => <PhotoContainer urls={this.state.search.urls} loading = {this.state.search.loading}/>} />
         </div>
       </BrowserRouter>
     );
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+class SearchForm extends Component{
+  state = {
+    submit: false
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.search(this.input.value);
+    e.currentTarget.reset();
 
-const SearchForm = () => {
+    this.setState({submit: true});
+  }
+  render(){
+    if(this.state.submit){
+      this.setState({
+        submit:false
+      });
+      return <Redirect to='/search' />
+    }
     return(
-        <form className="search-form">
-        <input type="search" name="search" placeholder="Search" required/>
+      <form className="search-form" onSubmit={this.handleSubmit}>
+        <input type="search" name="search" placeholder="Search" ref={(input) => this.input = input} required/>
         <button type="submit" className="search-button">
           <svg fill="#fff" height="24" viewBox="0 0 23 23" width="24" xmlns="http://www.w3.org/2000/svg">
             <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
@@ -12,5 +29,8 @@ const SearchForm = () => {
         </button>
       </form>
     );
+  }
+    
+  
 }
 export default SearchForm;
