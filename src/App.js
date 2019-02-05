9f6a42ea494c3750d7c44c2fb133ad6f112d079b
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-02-04 22:13:44 -0500

    routing, navlinks and loading state

--- src/App.js
+++ src/App.js
@@ -3,40 +3,69 @@ import SearchForm from './components/SearchForm';
 import MainNav from './components/MainNav';
 import PhotoContainer from './components/PhotoContainer';
 import apiKey from './config';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
 class App extends Component {
   state = {
-    searchedUrls:[],
-    catUrls:[],
-    dogUrls:[],
-    computerUrls:[]
+    searched:{
+      urls: [],
+      loading: true
+    },
+    cats:{
+      urls: [],
+      loading: true
+    },
+    dogs:{
+      urls: [],
+      loading: true
+    },
+    computers:{
+      urls: [],
+      loading: true
+    }
   }
   componentDidMount(){
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&format=json&nojsoncallback=1`)
       .then(res=>res.json())
       .then(res=>res.photos.photo.map(photo => `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`))
       .then(res=>this.setState({
-        catUrls:res
+        cats:{
+          urls: res,
+          loading: false
+        }
       }));
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=dogs&format=json&nojsoncallback=1`)
       .then(res=>res.json())
       .then(res=>res.photos.photo.map(photo => `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`))
       .then(res=>this.setState({
-        dogUrls:res
+        dogs:{
+          urls: res,
+          loading: false
+        }
       }));
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=computers&format=json&nojsoncallback=1`)
       .then(res=>res.json())
       .then(res=>res.photos.photo.map(photo => `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`))
       .then(res=>this.setState({
-        computerUrls:res
+        computers:{
+          urls: res,
+          loading: false
+        }
       }));
   }
   render() {
     return (
-      <div className="App">
-       <SearchForm />
-       <MainNav />
-       <PhotoContainer urls={this.state}/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <SearchForm />
+          <MainNav />
+          <Route path="/cats" render={() => <PhotoContainer urls={this.state.cats.urls} loading = {this.state.cats.loading}/>} />
+          <Route path="/dogs" render={() => <PhotoContainer urls={this.state.dogs.urls} loading = {this.state.dogs.loading}/>} />
+          <Route path="/computers" render={() => <PhotoContainer urls={this.state.computers.urls} loading = {this.state.computers.loading}/>} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-const PhotoContainer = ({urls}) => {
+const PhotoContainer = ({urls, loading}) => {
 
     const populate = ()=>{
-      return urls.catUrls.map((url,i) => <li key={i}>
+      let lis =  urls.map((url,i) => <li key={i}>
         <img src={url} alt="" />
       </li>)
+      if(lis.length === 0){
+        if(loading){
+          return(
+            <li className="not-found">
+              <h3>Loading...</h3>
+            </li>
+          );
+        }else{
+          return(
+            <li className="not-found">
+              <h3>No Results Found</h3>
+              <p>You search did not return any results. Please try again.</p>
+            </li>
+          );
+        }
+      }else{
+        return lis;
+      }
     }
     return(
         <div className="photo-container">
@@ -13,10 +31,7 @@ const PhotoContainer = ({urls}) => {
         <ul>
           {populate()}
           
-          <li className="not-found">
-            <h3>No Results Found</h3>
-            <p>You search did not return any results. Please try again.</p>
-          </li>
+          
         </ul>
       </div>
     );
