cdaf4adbded0102ec58ca65ee1c9a8219e134e9e
Yousef Mokaddem <yousefmokaddem96@gmail.com>
2019-02-06 01:06:18 -0500

    Updates

---
+++ precache-manifest.eddc0a987c18ae41d6f790795286084c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74afd3c412199fce67cb",
+    "url": "/react-gallery-app/static/css/main.55dab403.chunk.css"
+  },
+  {
+    "revision": "74afd3c412199fce67cb",
+    "url": "/react-gallery-app/static/js/main.74afd3c4.chunk.js"
+  },
+  {
+    "revision": "5b6d1abc0343ac28caa9",
+    "url": "/react-gallery-app/static/js/1.5b6d1abc.chunk.js"
+  },
+  {
+    "revision": "d4e6bb9232dacabf4f66",
+    "url": "/react-gallery-app/static/js/runtime~main.d4e6bb92.js"
+  },
+  {
+    "revision": "9ecdc8870e8188b71eb3a32148beadc9",
+    "url": "/react-gallery-app/index.html"
+  }
+];
